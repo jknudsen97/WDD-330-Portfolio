387e81fef8473740f5894626cd785af331da24ff
Jeremy Knudsen <jeremythenome@gmail.com>
2021-10-19 17:16:11 -0600

    commit

--- week2/js/script.js
+++
@@ -1,9 +0,0 @@
-const date = new Date();
-const options = {
-      weekday: "long",
-      month: "long",
-      day: "numeric",
-      year: "numeric",
-    };
-document.querySelector("#currentyear").innerHTML 
-= date.getFullYear();
