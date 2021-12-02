947e3792870543142336b95d24d18fb9b6f90585
Jeremy Knudsen <jeremythenome@gmail.com>
2021-12-02 14:45:11 -0700

    commit

---
+++ .history/week7/comments_20211202143941.js
@@ -0,0 +1,18 @@
+// Set variable to current date and time
+const bechlerFalls = {
+    name: "Bechler Falls",
+    date: new Date("June 26, 2016 1:24 PM"),
+    content: "Beautiful short hike along the Bechler river to Bechler Falls"
+  };
+
+  const  tetonCanyon = {
+    name: "Teton Canyon",
+    date: new Date("July 4 2016 2:30pm"),
+    content: "The hike is fairly challenging but worth the effort!"
+  };
+
+  const denandaFalls = {
+    name: "Denanda Falls",
+    date: new Date("Aug 16 2017 6:30pm"),
+    content: "The view of the falls is truly spectacular."
+  };
